c6c68411ce3cf18fdaa7d6ae86ee0fda7b1684ca
Kelly Egan <kelly.egan@gmail.com>
2016-03-17 08:27:46 -0400

    Create basic starter sketch.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
 //You should declare any variables here
-
+var xPosition, yPosition, diameter;
 
 //This function is run ONCE at the beginning
 function setup() {
+  createCanvas(640,480);
 
+  xPosition = width / 2;
+  yPosition = height / 2;
+  diameter = 50;
+
+  noStroke();
 }
 
 
 //This function is run CONTINUOUSLY in a loop
 function draw() {
-
+  background(200);
+  ellipse(xPosition, yPosition, diameter, diameter);
 }
 
 //I usually declare functions here
