58b1932f92618f38d840e33cd2a7b3cf3e987434
Kelly Egan <kelly.egan@gmail.com>
2016-03-16 22:14:41 -0400

    Add p5.js files

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//You should declare any variables here
+
+
+//This function is run ONCE at the beginning
+function setup() {
+
+}
+
+
+//This function is run CONTINUOUSLY in a loop
+function draw() {
+
+}
+
+//I usually declare functions here
